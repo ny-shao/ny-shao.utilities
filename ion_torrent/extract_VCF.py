110ca9ed0190dfaa72e72ed7d221bf234e71e991
sny <shaoningyi@gmail.com>
2015-06-02 10:53:54 -0700

    1. Add extract_VCF.py. 2. re-organize parameters.

--- ion_torrent/extract_VCF.py
+++ ion_torrent/extract_VCF.py
@@ -8,14 +8,27 @@ import requests
 import json
 import sys
 
-# Query list of resultName
-queries = [u'Auto_snxxxx_Chip_1',
-        u'Auto_snxxxx_Chip_2']
-
-# "ERCC_Analysis", "PartekFlowUploader", "FileExporter", "coverageAnalysis"
-query_plugins = [u'ERCC_Analysis']
+def download_file(url, resultName, sample):
+    ## Modified from:
+    ## http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+    local_filename = "_".join([resultName, sample, url.split('/')[-1]])
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True, auth=('ionuser', 'ionuser'))
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    return local_filename
 
 ts_server_add = "127.0.0.1" # address of Ion Torrent Server
+
+# Query list of resultName
+queries = [u'xxx_001',
+        u'xxx_002']
+
+# "ERCC_Analysis", "PartekFlowUploader", "FileExporter", "coverageAnalysis"
+query_plugins = [u'variantCaller']
 
 # Query round 1. Extract the total counts of the plugins. The first query only
 # return 20 elements.
@@ -35,13 +48,11 @@ pluginresults = ts_api_response["objects"]
 for pluginresult in pluginresults:
     if pluginresult[u'resultName'] in queries:
         resultName = pluginresult[u'resultName']
+        print(resultName)
         if pluginresult[u'pluginName'] in query_plugins:
             path = pluginresult[u'path'].replace("results/analysis/", "")
             for sample in pluginresult[u'store']['barcodes'].keys():
-                print sample
-                print("http://%s%s/%s/ercc.counts"%(ts_server_add, path, sample))
-                resp = requests.get("http://%s%s/%s/ercc.counts"%(ts_server_add, path, sample),
-                    auth=('ionuser', 'ionuser'))
-                out_f = file(resultName + "_" + sample + "_ERCC_counts.txt", 'w')
-                out_f.write(resp.content)
-                out_f.close()
+                for file in pluginresult[u'store'][u'files']:
+                    path = file['server_path'].replace("results/analysis/", "")
+                    url = "http://%s%s"%(ts_server_add, path)
+                    print(download_file(url, resultName, sample))
